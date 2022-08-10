53644c2b2b64649aeb659456e7b842d435547b36
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2022-08-10 14:13:10 +0200

    pb_tool change pluginpath
    this will need to work on the local appdata

--- hhnk_threedi_plugin/local_settings_default.py
+++ hhnk_threedi_plugin/local_settings_default.py
@@ -1,10 +1,9 @@
+#rename to local_settings.py
 import os 
 
 DEBUG=True
 
-hhnk_threedi_tools_path = r'E:\github\wvangerwen\hhnk-threedi-tools'
+hhnk_threedi_tools_path = None
 threeditoolbox_path = os.path.join(os.getenv('APPDATA'), r'3Di\QGIS3\profiles\default\python\plugins\ThreeDiToolbox\deps')
 
-
-
-project_path = r'E:\02.modellen\model_test_v2'
+project_path = None
