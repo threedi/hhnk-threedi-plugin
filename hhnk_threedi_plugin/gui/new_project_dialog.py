9b7abb4ed81baff97dfdf19435ee342f7d1eb35f
jkaptein <101330842+jkaptein@users.noreply.github.com>
2023-04-04 15:35:50 +0200

    reference
    new project - referentiemodel ingevoegd

--- hhnk_threedi_plugin/gui/new_project_dialog.py
+++ hhnk_threedi_plugin/gui/new_project_dialog.py
@@ -1,5 +1,10 @@
 import os
+import pandas as pd
+import shutil
+import glob 
+
 from PyQt5.QtWidgets import (
+    QComboBox,
     QDialog,
     QVBoxLayout,
     QFileDialog,
@@ -20,31 +25,54 @@ from ..error_messages.input_error_messages import (
 from hhnk_threedi_tools.core.folders import Folders
 
 
+
 def setupUi(new_project_dialog):
     layout = QVBoxLayout()
     layout.setAlignment(Qt.AlignTop)
     layout.setContentsMargins(25, 25, 25, 25)
     new_project_dialog.setWindowTitle("Nieuw project aanmaken")
     new_project_dialog.setMinimumWidth(275)
+    
     # Creates items to be in widget
     new_project_dialog.folder_selector = fileWidget(
         file_dialog_title="Selecteer map waarin project wordt aangemaakt",
         file_mode=QFileDialog.Directory,
         select_text="Selecteer map:",
     )
+    new_project_dialog.reference_model_name = QLabel("Kies referentie model:")
+    new_project_dialog.reference_model_dropdown = QComboBox()
+    
     new_project_dialog.polder_name_label = QLabel("Geef project (polder) naam op:")
     new_project_dialog.polder_name_field = QLineEdit()
+
     new_project_dialog.create_project_btn = QPushButton("Project aanmaken")
 
     # Add items to layout
     layout.addWidget(new_project_dialog.folder_selector, alignment=Qt.AlignTop)
     layout.addSpacerItem(QSpacerItem(20, 10, QSizePolicy.Expanding))
-    layout.addWidget(new_project_dialog.polder_name_label, alignment=Qt.AlignTop)
+
+    layout.addWidget(new_project_dialog.reference_model_name, alignment=Qt.AlignTop)
+    layout.addWidget(new_project_dialog.reference_model_dropdown, alignment=Qt.AlignTop)
+    layout.addSpacerItem(QSpacerItem(20, 10, QSizePolicy.Expanding))
+
+    layout.addWidget(new_project_dialog.polder_name_label, alignment=Qt.AlignTop)   
     layout.addWidget(new_project_dialog.polder_name_field, alignment=Qt.AlignTop)
     layout.addSpacerItem(QSpacerItem(20, 10, QSizePolicy.Expanding))
+
     layout.addWidget(new_project_dialog.create_project_btn, alignment=Qt.AlignTop)
     new_project_dialog.setLayout(layout)
 
+    #set reference model
+    base_path = r"E:\02.modellen"
+    reference_models_paths = glob.glob(str(base_path + "\\cbt-[0-9]")) + glob.glob(str(base_path + "\\cbt-[0-9][0-9]"))
+    for paths in reference_models_paths:
+        files = os.listdir(paths)
+        for file in files:
+            if file.endswith('.sqlite'):
+                new_project_dialog.reference_model_dropdown.addItem((os.path.splitext(file)[0]))
+    new_project_dialog.reference_model_dropdown.setStyleSheet("QComboBox { combobox-popup: 0; }")
+    new_project_dialog.reference_model_dropdown.setMaxVisibleItems(10)
+    
 
 class newProjectDialog(QDialog):
     """
@@ -74,12 +102,13 @@ class newProjectDialog(QDialog):
             iface.messageBar().pushMessage(
                 "Selecteer een map om nieuw project in aan te maken", Qgis.Critical
             )
+              
         else:
             if not os.path.isdir(base_path):
                 iface.messageBar().pushMessage(
                     "Selecteer een bestaande map om nieuw project in aan te maken",
                     Qgis.Critical,
-                )
+                )  
             else:
                 project_name = self.polder_name_field.text()
                 if not project_name:
@@ -92,18 +121,82 @@ class newProjectDialog(QDialog):
                         iface.messageBar().pushMessage(
                             folder_exists_already, Qgis.Critical
                         )
-                    else:
-                        try:
-                            os.mkdir(full_path)
-                            Folders(full_path, create=True)
-                            self.project_folder_path.emit(full_path)
-                            self.accept()
-                            QMessageBox.information(
-                                None, "Create project", "Your folders are created!"
-                            )
-                            self.full_path = full_path
-                        except Exception:
+                    else: 
+                        reference_model = str(self.reference_model_dropdown.currentText())    
+                        if not reference_model:
                             iface.messageBar().pushMessage(
-                                invalid_character_in_filename, Qgis.Critical
+                            "Geen referentie model opgegeven", Qgis.Critical
                             )
-                            pass
+                        else:
+                            try:
+                                print(full_path)
+                                os.mkdir(full_path)
+                                Folders(full_path, create=True)
+                                print("succes1")
+                                self.project_folder_path.emit(full_path)
+                                self.accept()
+                                QMessageBox.information(
+                                    None, "Create project", "Your folders are created!"
+                                )
+                                self.full_path = full_path                                
+
+                            except Exception:
+                                iface.messageBar().pushMessage(
+                                    invalid_character_in_filename, Qgis.Critical
+                                )
+
+                            else:
+                                try:
+                                    self.copy_files()
+                                except:
+                                    iface.messageBar().pushMessage(
+                                    "Settings, sqlite of rasters niet gekopieerd", Qgis.Warning
+                                        )  
+                                    pass
+
+
+
+    def copy_files(self):
+        #setting the paths
+        base_path = self.folder_selector.filePath()
+        project_name = self.polder_name_field.text()
+        dst = Folders(os.path.join(base_path, project_name))
+
+        reference_model_base = self.reference_model_dropdown.currentText()
+        reference_model = reference_model_base[4:]
+        bwn_paths = glob.glob(str(base_path + "\\cbt-[0-9]")) + glob.glob(str(base_path + "\\cbt-[0-9][0-9]"))
+        raster_paths = glob.glob(str(base_path + "\\cbt-[0-9]\\rasters")) + glob.glob(str(base_path + "\\cbt-[0-9][0-9]\\rasters"))      
+       
+        #adjust and copy model settings        
+        raw_model_settings = pd.read_excel("E:\\02.modellen\\model_test_v2\\02_schematisation\\model_settings.xlsx", engine="openpyxl")
+        new_model_settings = pd.DataFrame(raw_model_settings.replace(regex=['hoekje'], value=reference_model))
+        new_model_settings = pd.DataFrame(new_model_settings.replace(regex=['model_test_v2'], value=project_name))        
+        new_model_settings.to_excel(os.path.join(dst.model.base, "model_settings.xlsx"))
+
+        #copy model settings default file
+        model_settings_default = pd.read_excel("E:\\02.modellen\\model_test_v2\\02_schematisation\\model_settings_default.xlsx", engine="openpyxl")
+        model_settings_default.to_excel(os.path.join(dst.model.base, "model_settings_default.xlsx"))
+        
+        #searching sqlite file and copy to destination folder
+        for paths in bwn_paths:
+            files = os.listdir(paths)
+            copy_sqlite = []
+            for file in files:
+                if reference_model_base in file:
+                    if file.endswith('.sqlite'):
+                        copy_sqlite.append(os.path.join(paths, file))
+            for files in copy_sqlite:
+                shutil.copy(files, os.path.join(dst.model.schema_base.path))       
+        
+        #searching raster files and copy to destination folder
+        for paths in raster_paths:
+            files = os.listdir(paths)
+            copy_rasters = []
+            for file in files:
+                if reference_model in file:
+                        if file.endswith('.tif'):
+                            copy_rasters.append(os.path.join(paths, file))
+            for files in copy_rasters:
+                shutil.copy(files, os.path.join(dst.model.schema_base.rasters.path))
+
+                    
