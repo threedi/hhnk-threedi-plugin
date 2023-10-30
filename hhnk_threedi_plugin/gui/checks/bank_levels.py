3d3474952ec6eff578139ace4af8e9c5e4eb8bd3
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2023-10-30 09:38:23 +0100

    remove unused import

--- hhnk_threedi_plugin/gui/checks/bank_levels.py
+++ hhnk_threedi_plugin/gui/checks/bank_levels.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from qgis.core import Qgis
 from hhnk_threedi_plugin.gui.utility.file_widget import fileWidget
 
-from hhnk_threedi_plugin.error_messages.input_error_messages import invalid_model_path
 
 from hhnk_threedi_plugin.gui.checks.bank_levels_widgets.proposed_changes_dialog import modelChangesDialog
 import hhnk_threedi_plugin.tasks.task_bank_levels as task_bank_levels
