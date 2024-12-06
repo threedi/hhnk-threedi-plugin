85ad21c03f02853c3c8ba75b592ef1f05ee410ce
Overmeen <84539501+Overmeen@users.noreply.github.com>
2024-12-06 10:02:24 +0100

    Work in progress

--- hhnk_threedi_plugin/gui/schematisation_builder.py
+++ hhnk_threedi_plugin/gui/schematisation_builder.py
@@ -3,7 +3,24 @@ from hhnk_threedi_plugin.hhnk_toolbox_dockwidget import HHNK_toolboxDockWidget
 
 @dataclass
 class SchematisationBuilder:
-    """All actions in the schematisation_builder tab of the HHNK 3Di Toolbox."""
+    """
+    All actions in the schematisation_builder tab of the HHNK 3Di Toolbox.
+
+    UI includes (from top to down):
+    SelectProjectLabel : QLabel
+    SelectProjectComboBox : QComboBox
+    CreateProjectLabel : QLabel
+    CreateProjectPlainTextEdit : QPlainTextEdit
+    CreateProjectPushButton : QPushButton
+    SchematisationBuilderVerticalSpacer : Spacer
+    ProjectTabWidget : QTabWidget
+        tab_status_0 : QWidget
+            SelectPolderLabel : QLabel
+            SelectPolderFileWidget : QgsFileWidget
+            ExportDAMOandHyDAMOPushButton : QPushButton
+        tab_status_1 : QWidget
+            ValidatePushButton : QPushButton    
+    """
     dockwidget: HHNK_toolboxDockWidget
 
     def __post_init__(self):
